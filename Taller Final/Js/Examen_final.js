221077e794a709f76307e98d3573252b73d3f843
Juanda-20 <125904917+Juanda-20@users.noreply.github.com>
2023-03-28 06:44:28 -0500

    Add files via upload

---
+++ Taller Final/Js/Examen_final.js
@@ -0,0 +1,15 @@
+
+
+var num1 = parseInt(prompt("Digite el primer número a multiplicar: "))
+var num2 = parseInt(prompt("Digite el segundo número a multiplicar: "))
+let num
+
+for (let num = num1; num <= num2; num++){
+    for (let j = 1; j <= 10; j++) {
+        if (num2 >10){
+           }else
+            console.log(num + " x " + j + " = " + num * j)
+    }
+
+    console.log("\n")
+}
